a6afc2de8ef75651381ba188917c53011eabfc69
Martin Woxneryd <martin@woxneryd.com>
2009-01-28 13:17:56 +0100

    Clean up

--- src/AlienNumbers.groovy
+++ src/AlienNumbers.groovy
@@ -4,9 +4,8 @@ assert f('13', '0123456789abcdef', '01') == '10011'
 assert f('CODE', 'O!CDE?', 'A?JM!.') == 'JAM!'
 
 String f(String val, String s, String t) {
-    def c
     int v = 0
-    val.reverse().eachWithIndex(){ch,i->v+=s.length()**i*s.indexOf(ch)}
+    val.reverse().eachWithIndex{ch,i->v+=s.length()**i*s.indexOf(ch)}
     c={res->r=v%t.length();v=v/t.length();v==0?t[r]:res+t[r]+c(res);}
     return c('').reverse()
 }
